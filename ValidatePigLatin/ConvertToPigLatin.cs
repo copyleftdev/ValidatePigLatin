411745b86b667e1c180d434f2a14157e3244c05b
Don Johnson <dj@codetestcode.io>
2015-06-14 22:49:44 -0700

    Initial Commit

---
+++ ValidatePigLatin/ConvertToPigLatin.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ValidatePigLatin
+{
+    class ConvertToPigLatin
+    {
+        bool IsVowel(char chr)
+        {
+            if (chr == 'a'  || chr == 'e' || chr == 'i' || chr == 'o' || chr == 'u' || chr == 'y' || chr == 'A' || chr == 'E' || chr == 'I' || chr == 'O' || chr == 'U' || chr == 'Y')
+                return true;
+            else
+                return false;
+        }
+
+        bool ConvertWordToPigLatin(String strIn, ref String strOut)
+        {
+            if (strIn.Length <= 0)
+                return false;
+
+            if (IsVowel(strIn[0]))
+            {
+                strOut += strIn.Substring(1, strIn.Length - 1);
+                strOut += strIn[0];
+            }
+            else
+            {
+                int i = 0;
+                String tmpStr = "";
+                while (!IsVowel(strIn[i]))
+                {
+                    tmpStr += strIn[i];
+                    ++i;
+                }
+                strOut += strIn.Substring(i);
+                strOut += tmpStr;
+            }
+            if (strOut == strOut.ToLower()) {
+                strOut += "ay";
+            }
+            else if (strOut == strOut.ToUpper())
+            {
+                strOut += "AY";
+            }
+            else
+            {
+                strOut += "ay";
+            }
+            
+            return true;
+        }
+
+        public bool ConvertStringToPigLatin(String strIn, ref String strOut)
+        {
+            if (strIn.Length <= 0)
+                return false;
+
+            int wordDelimiter = -1,
+                stringIndex = 0;
+
+            do
+            {
+                wordDelimiter = strIn.IndexOf(' ', stringIndex);
+                int end = wordDelimiter > 0 ? wordDelimiter - stringIndex : strIn.Length - stringIndex;
+                string tmpWord = strIn.Substring(stringIndex, end);
+                if (!ConvertWordToPigLatin(tmpWord, ref strOut))
+                    return false;
+                if (wordDelimiter > 0)
+                    strOut += " ";
+                stringIndex = wordDelimiter + 1;
+            } while (wordDelimiter > 0);
+
+            return true;
+        }
+    }
+}
---
+++ ValidatePigLatin/ValidatePigLatin.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Provided is a function that will take in a string, and output this string in Pig Latin
+ * according to the following rules:
+ * Rules of Pig Latin:
+ * 1. If the string starts with a consonant, all leading consonants are moved to the end
+ *	 of the string
+ * 2. If the string starts with a vowel (a,e,i,o,u,y) the first vowel is moved to the end
+ *	 of the string
+ * 3. "ay" is always added to the end of the string
+ *
+ * This function has a number of defects.
+ *
+ * Step 1: Privide test input to this function to discover the defects.  The code is not
+ * to be referred to during this step (i.e. Black box testing).
+ *
+ * Step 2: Debug to code and resolve the issues found in Step 1 (code can be referred to
+ * in this step).
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ValidatePigLatin
+{
+    class ValidatePigLatin
+    {
+        static void Main(string[] args)
+        {
+            ConvertToPigLatin c = new ConvertToPigLatin();
+            String output = "",
+              input = "I need a Vacation";
+
+            if (c.ConvertStringToPigLatin(input,  ref output))
+            {
+                Console.WriteLine("Input: " + input);
+                Console.WriteLine("PigLatin: " + output);
+            }
+            else
+                Console.WriteLine("Failed.");
+
+            return;
+        }
+    }
+}
